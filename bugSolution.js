0053990c4f1baf46dad7b033b61ceee57e8686c6
BugHunter-X <askzenai@gmail.com>
2025-02-05 12:48:09 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  if(typeof a !== 'number' || typeof b !== 'number'){
+    throw new Error('Operands must be numbers');
+  }
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return new Error('Invalid operator');
+  }
+}
+
+console.log(operate('+', 5, 3)); // 8
+console.log(operate('-', 5, 3)); // 2
+console.log(operate('*', 5, 3)); // 15
+console.log(operate('/', 5, 3)); // 1.6666666666666667
+console.log(operate('/', 5, 0)); // Error: Cannot divide by zero
+console.log(operate('%', 5, 3));//Error: Invalid operator
+console.log(operate('+', 'hello', 3));//Error:Operands must be numbers
